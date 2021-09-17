558c5831a0f3d52c1cc1d557a99dd880b8473b43
Sam <samuv.eth@gmail.com>
2021-09-17 20:44:18 +0700

    Add defaultParams to chart plugin

--- src/plugins/charts/index.ts
+++ src/plugins/charts/index.ts
@@ -2,4 +2,15 @@ export default class Plugin {
   public author = 'zerquix18';
   public version = '0.1.0';
   public name = 'Charts';
+  public defaultParams = {
+    total_votes_per_day: {
+      enabled: true
+    },
+    voting_power_per_day: {
+      enabled: true
+    },
+    voting_power_per_address: {
+      enabled: true
+    }
+  };
 }
